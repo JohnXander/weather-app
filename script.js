b6b56aaa6364194066863e783d145902574b238c
John Blox <103267235+JohnXander@users.noreply.github.com>
2022-04-17 20:30:29 +0000

    js linked to html

--- script.js
+++ script.js
@@ -1,16 +1,51 @@
+const main = document.getElementById("main");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+
 const apikey = "3265874a2c77ae4a04bb96236a642d2f";
 
-const url = (location) =>
-    `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apikey}`;
+const url = (city) =>
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`;
 
 
-async function getWeatherByLocation(location) {
-    const resp = await fetch(url(location), {
+async function getWeatherByLocation(city) {
+    const resp = await fetch(url(city), {
         origin: "cors"
     });
     const respData = await resp.json();
 
-    console.log(respData)
+    addWeatherToPage(respData);
 }
 
-getWeatherByLocation("London");
+function addWeatherToPage(data) {
+    const temp = KtoC(data.main.temp);
+
+    const weather = document.createElement("div");
+    weather.classList.add("weather");
+
+    weather.innerHTML = `
+        <small>It is </small>
+        <h2>${temp}°C</h2>
+        <p>in ${search.value}</p>
+    `;
+
+    // clean up
+
+    main.innerHTML = "";
+
+    main.appendChild(weather);
+}
+
+function KtoC(K) {
+    return (K - 273.15).toFixed(2);
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const city = search.value;
+
+    if (city) {
+        getWeatherByLocation(city);
+    }
+});
